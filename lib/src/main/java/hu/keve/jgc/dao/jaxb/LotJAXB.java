e77c293c1cf1747a94c152abed9e10d7487c0562
kevemueller <keve@keve.hu>
2018-06-07 19:39:55 +0200

    maven improvements

--- lib/src/main/java/hu/keve/jgc/dao/jaxb/LotJAXB.java
+++ lib/src/main/java/hu/keve/jgc/dao/jaxb/LotJAXB.java
@@ -1,6 +1,13 @@
 package hu.keve.jgc.dao.jaxb;
 
+import hu.keve.jgc.dao.Account;
 import hu.keve.jgc.dao.Lot;
 
 public interface LotJAXB extends Lot {
+	default Account getAccount() {
+		return null;
+	}
+	default boolean isClosed() {
+		return false;
+	}
 }
